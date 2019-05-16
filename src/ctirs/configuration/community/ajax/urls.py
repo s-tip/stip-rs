bcecca0a708ea7d14e11f1be41e7501f6951aa30
ykoji8681 <ykoji8681@gmail.com>
2019-05-16 01:13:52 +0000

    fix missing community dir

---
+++ src/ctirs/configuration/community/ajax/urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+#URLを正規表現で評価し、マッチングした場合の処理箇所を定義
+from django.conf.urls import url
+import ctirs.configuration.community.ajax.views as ajax
+
+urlpatterns = [
+    #test_webhook
+    url(r'^test_webhook$', ajax.test_webhook),
+]
+
