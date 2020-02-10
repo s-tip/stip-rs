dfa41eced3ab2d625f5c1a8f90dd023a6a86e04b
ykoji8681 <ykoji8681@gmail.com>
2020-02-10 15:46:04 +0900

    Fix iSIGHTPartners Adapter bug

--- src/ctirs/core/adapter/__init__.py
+++ src/ctirs/core/adapter/__init__.py
@@ -7,7 +7,7 @@ from ctirs.core.stix.regist import regist
 def _regist_stix(content, community, via):
     # stixファイルを一時ファイルに出力
     stix_file_path = tempfile.mktemp(suffix='.xml')
-    with open(stix_file_path, 'wb+') as fp:
+    with open(stix_file_path, 'w+t', encoding='utf-8') as fp:
         # cb.contentがstixの中身(contentの型はstr)
         fp.write(content)
     # 登録
