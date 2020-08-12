a8eb22e069b7495d9c10418637c34e7aab7b753e
Toshitaka Satomi <satomi.toshitak@fujitsu.com>
2020-08-12 10:07:50 +0900

    Fixed names of user configuration ajax endpoint

--- src/ctirs/configuration/user/ajax/views.py
+++ src/ctirs/configuration/user/ajax/views.py
@@ -9,48 +9,83 @@ def get_configuration_user_ajax_change_auth_username(request):
     return get_text_field_value(request, 'username', default_value='')
 
 
-def get_configuration_user_ajax_change_auth_key(request):
-    return get_text_field_value(request, 'key', default_value='')
-
-
 def get_configuration_user_ajax_change_auth_value(request):
     return get_text_field_value(request, 'value', default_value='')
+
+
+def get_configuration_user_ajax_change_active_value(request):
+    return get_text_field_value(request, 'value', default_value='')
+
+
+def check_ajax_request(request):
+    if request.method != 'GET':
+        return {
+            'status': 'NG',
+            'message': 'Invalid HTTP method'}
+    if not request.user.is_active:
+        return {
+            'status': 'NG',
+            'message': 'You account is inactive.'}
+    if not request.user.is_admin:
+        return {
+            'status': 'NG',
+            'message': 'No permission.'}
+    return None
 
 
 @login_required
 @csrf_protect
 def change_auth(request):
-    # GET以外はエラー
-    if request.method != 'GET':
-        r = {'status': 'NG',
-             'message': 'Invalid HTTP method'}
+    r = check_ajax_request(request)
+    if r:
         return JsonResponse(r, safe=False)
-    # activeユーザー以外はエラー
-    if not request.user.is_active:
-        r = {'status': 'NG',
-             'message': 'You account is inactive.'}
-        return JsonResponse(r, safe=False)
-    # is_admin権限なしの場合はエラー
-    if not request.user.is_admin:
-        r = {'status': 'NG',
-             'message': 'No permission.'}
-        return JsonResponse(r, safe=False)
-
     try:
         username = get_configuration_user_ajax_change_auth_username(request)
-        key = get_configuration_user_ajax_change_auth_key(request)
         value = True if get_configuration_user_ajax_change_auth_value(request) == 'true' else False
-
         u = STIPUser.objects.get(username=username)
-        # keyに応じて属性の値を変更
-        if(key == 'is_active'):
-            u.is_active = value
-        elif(key == 'is_admin'):
-            u.is_admin = value
-            u.is_superuser = value
-        elif(key == 'totp_sercet'):
-            u.totp_secret = None
-        # 変更を保存
+        u.is_admin = value
+        u.is_superuser = value
+        u.save()
+        r = {'status': 'OK',
+             'message': 'Success'}
+    except Exception as e:
+        r = {'status': 'NG',
+             'message': str(e)}
+    finally:
+        return JsonResponse(r, safe=False)
+
+
+@login_required
+@csrf_protect
+def change_active(request):
+    r = check_ajax_request(request)
+    if r:
+        return JsonResponse(r, safe=False)
+    try:
+        username = get_configuration_user_ajax_change_auth_username(request)
+        value = True if get_configuration_user_ajax_change_active_value(request) == 'true' else False
+        u = STIPUser.objects.get(username=username)
+        u.is_active = value
+        u.save()
+        r = {'status': 'OK',
+             'message': 'Success'}
+    except Exception as e:
+        r = {'status': 'NG',
+             'message': str(e)}
+    finally:
+        return JsonResponse(r, safe=False)
+
+
+@login_required
+@csrf_protect
+def unset_mfa(request):
+    r = check_ajax_request(request)
+    if r:
+        return JsonResponse(r, safe=False)
+    try:
+        username = get_configuration_user_ajax_change_auth_username(request)
+        u = STIPUser.objects.get(username=username)
+        u.totp_secret = None
         u.save()
         r = {'status': 'OK',
              'message': 'Success'}
