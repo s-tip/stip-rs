b9e7e1adcd5c4e5e1b7b9f28ec0dba706f3c8595
Toshitaka Satomi <satomi.toshitak@fujitsu.com>
2020-09-16 17:35:07 +0900

    Change setup_env.py to Add CR+LF code in .env

--- bin/setup_env.py
+++ bin/setup_env.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     except UndefinedValueError:
         secret_key = get_random_secret_key()
         with open(args.path, 'a') as fp:
-            fp.write('SECRET_KEY=\'%s\'' % (secret_key))
+            fp.write('SECRET_KEY=\'%s\'\n' % (secret_key))
         print('Set a SECRET_KEY value into .env (%s)' % (secret_key))
 
     sys.exit(0)
